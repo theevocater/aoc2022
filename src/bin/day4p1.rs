d30fb9451b82547d547c73b69350b79d9af11174
Jake Kaufman <me@jake.computer>
2022-12-04 20:00:41 -0500

    part 1 i'm an idiot
    spent 20 minutes debugging le vs ls

---
+++ src/bin/day4p1.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::io::BufRead;
+
+//39-80 36-69
+fn calculate(ls: i32, le: i32, rs: i32, re: i32) -> bool {
+    (ls <= rs && le >= re) || (rs <= ls && re >= le)
+}
+
+fn main() {
+    let inputs: Vec<String> = io::stdin().lock().lines().flatten().collect();
+    println!(
+        "{}",
+        inputs
+            .iter()
+            .map(|line| {
+                let split = line
+                    .splitn(2, ',')
+                    .map(|pair| pair.splitn(2, '-'))
+                    .flatten()
+                    .map(|c| c.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+                println!(
+                    "{}-{},{}-{}: {}",
+                    split[0],
+                    split[1],
+                    split[2],
+                    split[3],
+                    calculate(split[0], split[1], split[2], split[3])
+                );
+                calculate(split[0], split[1], split[2], split[3])
+            })
+            .filter(|f| *f)
+            .count()
+    )
+}
