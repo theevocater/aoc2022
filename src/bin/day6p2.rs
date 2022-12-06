7261afb5eee14404a8b6a2ba5c59337667500211
Jake Kaufman <me@jake.computer>
2022-12-06 15:52:37 -0500

    Make fancy

--- src/bin/day6p2.rs
+++ src/bin/day6p2.rs
@@ -3,23 +3,20 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
-    let inputs: Vec<String> = io::stdin().lock().lines().flatten().collect();
-    let window_size = 14;
-    inputs.iter().for_each(|input| {
+    io::stdin().lock().lines().flatten().for_each(|input| {
         println!(
-            "{:?}",
+            "{}",
             input
                 .chars()
                 .enumerate()
                 .collect::<Vec<(usize, char)>>()
-                .windows(window_size)
+                .windows(14)
                 .find(|window| {
                     let mut s = HashSet::new();
-                    window.iter().for_each(|a| {
-                        s.insert(a.1);
-                    });
-                    s.len() == window_size
+                    window.iter().all(|a| s.insert(a.1))
                 })
+                .and_then(|a| a.last().map(|a| a.0 + 1))
+                .expect("Couldn't find anything")
         );
     })
 }
